b9a41c7ec712f8fe926088b97223fc04ed880daa
dpolo-jamar <44912504+DanielPoloC@users.noreply.github.com>
2022-06-01 21:49:44 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 app = FastAPI()
 
 
 @app.get("/credits")
 async def credits():
-    return {"Developers": ['Daniel Polo', 'Carlos Rodriguez']}
+    return {
+        "Developers": [
+            'Daniel Polo',
+            'Carlos Rodriguez'
+        ],
+        'Database': os.environ.get('DATABASE_URL')
+    }
